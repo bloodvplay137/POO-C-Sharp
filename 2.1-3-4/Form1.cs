0c90f94147d6138375718329e58565323fe7cc65
Juan <juan.ahumada94@gmail.com>
2020-02-20 21:17:24 -0600

    Ejericio 3

---
+++ 2.1-3-4/Form1.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace _2._1_3_4
+{
+    public partial class frmCalcularTiempo : Form
+    {
+        //Objeto
+        Rectangulo pared = new Rectangulo();
+
+        public frmCalcularTiempo()
+        {
+            InitializeComponent();
+        }
+
+        private void frmCalcularTiempo_Load(object sender, EventArgs e)
+        {
+
+        }
+        double CalcularTiempo()
+        {
+            /*
+             * 1m**2 = 10 min
+             * area = ?
+             * aplicar regla de 3 simple...
+             */
+            return pared.CalcularArea() * 10;
+        }
+
+        private void btnCalcular_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show($"El area de su pared es de: {pared.CalcularArea()}m^2 y el tiempo que le llevara pintar su pared es de: {CalcularTiempo()}","Area",MessageBoxButtons.OK,MessageBoxIcon.Information);
+
+            
+        }
+
+        private void btnCapturar_Click(object sender, EventArgs e)
+        {
+            if (txtLargo.Text == "" || txtAncho.Text == "")
+            {
+                MessageBox.Show("Favor de capturar ambos datos.", "Atencion!", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }else
+            {
+                pared.ModificarLargo(double.Parse(txtLargo.Text));
+                lblLargo.Text = $"Largo Actual: {pared.ConsultarLargo()}";
+                txtLargo.Clear();
+                pared.ModificarAncho(double.Parse(txtAncho.Text));
+                lblAncho.Text = $"Ancho Actual: {pared.ConsultarAncho()}";
+                txtAncho.Clear();
+            }
+        }
+    }
+}
---
+++ 2.1-3-4/Rectangulo.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _2._1_3_4
+{
+    class Rectangulo
+    {
+        //atributos privados
+        private double ancho,largo;
+        
+        //metodos
+        public double CalcularArea()
+        {
+            // area rectangulo = b x h
+            return ancho * largo;
+        }    
+        //mutators
+        public void ModificarLargo(double largo)
+        {
+            this.largo = largo;
+        }
+        public void ModificarAncho(double ancho)
+        {
+            this.ancho = ancho;
+        }
+        //accesors
+        public double ConsultarLargo()
+        {
+            return largo;
+        }
+        public double ConsultarAncho()
+        {
+            return ancho;
+        }
+    }
+}
